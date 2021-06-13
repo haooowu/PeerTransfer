ccc8a0f641cea63f37b609630342fc2ffe436d1e
Hao W <haooo.wu@gmail.com>
2021-06-13 15:51:59 -0400

    hellow world

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const HelloWorld: React.FC = () => {
   return (
     <>
-  <p>TODO: start project setup</p>
-        <p>Hello World</p>
+      <p>TODO: start project setup</p>
+      <p>Hello World</p>
     </>
-  )
+  );
 };
 
 export default HelloWorld;
