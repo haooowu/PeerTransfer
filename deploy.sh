45e15c0ca9447a684c26abffd51c76b730f6313a
Hao W <haooo.wu@gmail.com>
2021-07-20 23:27:08 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,14 @@ then
   echo "Select sub folder for deploy"
   exit 1
 fi
+
 git checkout deploy
 git pull origin main
 git rm -r --cached .
 git add .
 
 yarn run build
-git add -A
-git commit -m "update build"
-git push origin
+git add build && git commit -m "update build"
 git subtree push --prefix $1 origin gh-pages
 
 git checkout main
