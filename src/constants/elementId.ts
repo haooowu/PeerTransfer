6bc65763d2310d01b1ff50b5e04d49d3e4537d30
Hao W <haooo.wu@gmail.com>
2021-08-03 17:26:41 -0400

    update toast and about message

---
+++ src/constants/elementId.ts
@@ -0,0 +1,2 @@
+export const ELE_PUBLIC_ID = 'public-id';
+export const ELE_PUBLIC_ID_WRAPPER = 'public-id-wrapper';
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from 'src/constants/collectionName';
 export * from 'src/constants/sessionStorage';
 export * from 'src/constants/localStorage';
 export * from 'src/constants/numericValues';
+export * from 'src/constants/elementId';
