c04afd43af7b8aa9604630fd323a602455baff76
Hao W <haooo.wu@gmail.com>
2021-10-17 22:19:44 -0400

    update firebase and add preconnect link

--- src/utils/getRandomFaceEmoji.ts
+++ src/utils/getRandomFaceEmoji.ts
@@ -1,6 +1,5 @@
-const emojiArr: string[] = [];
-
 const getRandomFaceEmoji = () => {
+  const emojiArr: string[] = [];
   let targetEmoji: string;
   for (let i = 0x1f600; i <= 0x1f64f; i += 1) emojiArr.push(String.fromCodePoint(i));
   let random = Math.floor(Math.random() * emojiArr.length);
